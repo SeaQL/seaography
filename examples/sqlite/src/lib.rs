9e9a9c154c5fcd755e14f086cbd4768092c36083
Chris Tsang <chris.2y3@outlook.com>
2022-09-11 18:27:20 +0800

    Regenerate sqlite example

--- examples/sqlite/src/lib.rs
+++ examples/sqlite/src/lib.rs
@@ -1,7 +1,10 @@
 use sea_orm::prelude::*;
+
 pub mod entities;
 pub mod query_root;
+
 pub use query_root::QueryRoot;
+
 pub struct OrmDataloader {
     pub db: DatabaseConnection,
 }
--- examples/sqlite/src/main.rs
+++ examples/sqlite/src/main.rs
@@ -4,20 +4,35 @@ use async_graphql::{
     EmptyMutation, EmptySubscription, Schema,
 };
 use async_graphql_poem::GraphQL;
-use generated::*;
+use dotenv::dotenv;
 use poem::{get, handler, listener::TcpListener, web::Html, IntoResponse, Route, Server};
 use sea_orm::Database;
+use seaography_example_sqlite::*;
+use std::env;
+
 #[handler]
 async fn graphql_playground() -> impl IntoResponse {
     Html(playground_source(GraphQLPlaygroundConfig::new("/")))
 }
+
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL environment variable not set");
+    let depth_limit = env::var("DEPTH_LIMIT")
+        .map(|data| data.parse::<usize>().expect("DEPTH_LIMIT is not a number"))
+        .map_or(None, |data| Some(data));
+    let complexity_limit = env::var("COMPLEXITY_LIMIT")
+        .map(|data| {
+            data.parse::<usize>()
+                .expect("COMPLEXITY_LIMIT is not a number")
+        })
+        .map_or(None, |data| Some(data));
     tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
+        .with_max_level(tracing::Level::INFO)
         .with_test_writer()
         .init();
-    let database = Database::connect("sqlite://chinook.db").await.unwrap();
+    let database = Database::connect(db_url).await.unwrap();
     let orm_dataloader: DataLoader<OrmDataloader> = DataLoader::new(
         OrmDataloader {
             db: database.clone(),
@@ -26,8 +41,18 @@ async fn main() {
     );
     let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
         .data(database)
-        .data(orm_dataloader)
-        .finish();
+        .data(orm_dataloader);
+    let schema = if let Some(depth) = depth_limit {
+        schema.limit_depth(depth)
+    } else {
+        schema
+    };
+    let schema = if let Some(complexity) = complexity_limit {
+        schema.limit_complexity(complexity)
+    } else {
+        schema
+    };
+    let schema = schema.finish();
     let app = Route::new().at("/", get(graphql_playground).post(GraphQL::new(schema)));
     println!("Playground: http://localhost:8000");
     Server::new(TcpListener::bind("0.0.0.0:8000"))
