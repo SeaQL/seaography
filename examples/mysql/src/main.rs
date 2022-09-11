fae2f968c101340888ed00667b52030650f2fd05
Chris Tsang <chris.2y3@outlook.com>
2022-09-11 21:04:00 +0800

    Regen MySQL example

--- examples/mysql/src/main.rs
+++ examples/mysql/src/main.rs
@@ -1,1 +1,62 @@
-use async_graphql :: { http :: { playground_source , GraphQLPlaygroundConfig } , EmptyMutation , EmptySubscription , Schema , dataloader :: DataLoader } ; use async_graphql_poem :: GraphQL ; use poem :: { get , handler , listener :: TcpListener , web :: Html , IntoResponse , Route , Server } ; use sea_orm :: Database ; use generated :: * ; # [handler] async fn graphql_playground () -> impl IntoResponse { Html (playground_source (GraphQLPlaygroundConfig :: new ("/"))) } # [tokio :: main] async fn main () { tracing_subscriber :: fmt () . with_max_level (tracing :: Level :: DEBUG) . with_test_writer () . init () ; let database = Database :: connect ("mysql://root:root@127.0.0.1/sakila") . await . unwrap () ; let orm_dataloader : DataLoader < OrmDataloader > = DataLoader :: new (OrmDataloader { db : database . clone () } , tokio :: spawn) ; let schema = Schema :: build (QueryRoot , EmptyMutation , EmptySubscription) . data (database) . data (orm_dataloader) . finish () ; let app = Route :: new () . at ("/" , get (graphql_playground) . post (GraphQL :: new (schema))) ; println ! ("Playground: http://localhost:8000") ; Server :: new (TcpListener :: bind ("0.0.0.0:8000")) . run (app) . await . unwrap () ; }
+use async_graphql::{
+    dataloader::DataLoader,
+    http::{playground_source, GraphQLPlaygroundConfig},
+    EmptyMutation, EmptySubscription, Schema,
+};
+use async_graphql_poem::GraphQL;
+use dotenv::dotenv;
+use poem::{get, handler, listener::TcpListener, web::Html, IntoResponse, Route, Server};
+use sea_orm::Database;
+use seaography_example_mysql::*;
+use std::env;
+
+#[handler]
+async fn graphql_playground() -> impl IntoResponse {
+    Html(playground_source(GraphQLPlaygroundConfig::new("/")))
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL environment variable not set");
+    let depth_limit = env::var("DEPTH_LIMIT")
+        .map(|data| data.parse::<usize>().expect("DEPTH_LIMIT is not a number"))
+        .map_or(None, |data| Some(data));
+    let complexity_limit = env::var("COMPLEXITY_LIMIT")
+        .map(|data| {
+            data.parse::<usize>()
+                .expect("COMPLEXITY_LIMIT is not a number")
+        })
+        .map_or(None, |data| Some(data));
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_test_writer()
+        .init();
+    let database = Database::connect(db_url).await.unwrap();
+    let orm_dataloader: DataLoader<OrmDataloader> = DataLoader::new(
+        OrmDataloader {
+            db: database.clone(),
+        },
+        tokio::spawn,
+    );
+    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
+        .data(database)
+        .data(orm_dataloader);
+    let schema = if let Some(depth) = depth_limit {
+        schema.limit_depth(depth)
+    } else {
+        schema
+    };
+    let schema = if let Some(complexity) = complexity_limit {
+        schema.limit_complexity(complexity)
+    } else {
+        schema
+    };
+    let schema = schema.finish();
+    let app = Route::new().at("/", get(graphql_playground).post(GraphQL::new(schema)));
+    println!("Playground: http://localhost:8000");
+    Server::new(TcpListener::bind("0.0.0.0:8000"))
+        .run(app)
+        .await
+        .unwrap();
+}
