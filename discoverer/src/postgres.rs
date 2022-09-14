ceb973928f57df156f070be84f8a59669312c1b3
Billy Chan <ccw.billy.123@gmail.com>
2022-09-14 17:37:39 +0800

    PostgreSQL parsing "currentSchema" from DB URL

--- discoverer/src/postgres.rs
+++ discoverer/src/postgres.rs
@@ -5,16 +5,14 @@ use sqlx::PgPool;
 pub async fn explore_postgres(uri: &str) -> Result<TablesHashMap> {
     let connection = PgPool::connect(uri).await?;
 
-    let database = uri
-        .split('/')
-        .last()
-        .ok_or("database not specified in url")?;
+    let db_uri = url::Url::parse(uri).expect("Fail to parse database URL");
 
-    let schema = database.split("currentSchema=").last().unwrap_or("public");
+    let schema = db_uri
+        .query_pairs()
+        .find(|(k, _)| k == "currentSchema")
+        .map_or("public".to_string(), |(_, v)| v.to_string());
 
-    let schema = if schema.is_empty() { "public" } else { schema };
-
-    let schema_discovery = SchemaDiscovery::new(connection, schema);
+    let schema_discovery = SchemaDiscovery::new(connection, &schema);
 
     let schema = schema_discovery.discover().await;
 
