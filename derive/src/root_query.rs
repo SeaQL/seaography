58b274e544b812cab281f85a83b080e80c5ace44
Panagiotis Karatakis <karatakis@csd.auth.gr>
2022-10-10 22:33:03 +0300

    Unify cursor and page pagination

--- derive/src/root_query.rs
+++ derive/src/root_query.rs
@@ -1,4 +1,3 @@
-use heck::ToUpperCamelCase;
 use proc_macro2::{Ident, TokenStream};
 use quote::{format_ident, quote};
 
@@ -24,47 +23,23 @@ pub fn root_query_fn(
         })
         .collect::<Result<Vec<TokenStream>, crate::error::Error>>()?;
 
-    let names: Vec<TokenStream> = paths
-        .iter()
-        .map(|path| {
-            let name = path.clone().into_iter().last().unwrap().to_string();
-            let name = format!("Paginated{}Result", name.to_upper_camel_case());
-
-            quote! {
-                #[graphql(concrete(name = #name, params(#path::Model)))]
-            }
-        })
-        .collect();
-
     let queries: Vec<TokenStream> = paths
         .iter()
         .map(|path| {
             let name = format_ident!("{}", path.clone().into_iter().last().unwrap().to_string());
-            let name_cursor = format_ident!(
-                "{}_cursor",
-                path.clone().into_iter().last().unwrap().to_string()
-            );
 
             let basic_query = basic_query(&name, path);
 
-            let cursor_query = cursor_query(&name_cursor, path);
-
             quote! {
                 #basic_query
-
-                #cursor_query
             }
         })
         .collect();
 
-    let basic_dependencies = basic_dependencies(names);
-
-    let cursor_dependencies = cursor_dependencies();
+    let basic_dependencies = basic_dependencies();
 
     Ok(quote! {
         #basic_dependencies
-
-        #cursor_dependencies
 
         #[async_graphql::Object]
         impl #ident {
@@ -79,10 +54,13 @@ pub fn basic_query(name: &Ident, path: &TokenStream) -> TokenStream {
             &self,
             ctx: &async_graphql::Context<'a>,
             filters: Option<#path::Filter>,
-            pagination: Option<PaginationInput>,
+            pagination: Option<Pagination>,
             order_by: Option<#path::OrderBy>,
-        ) -> PaginatedResult<#path::Model> {
+        ) -> async_graphql::types::connection::Connection<String, #path::Model, ExtraPaginationFields, async_graphql::types::connection::EmptyFields> {
             use sea_orm::prelude::*;
+            use sea_orm::Iterable;
+            use seaography::itertools::Itertools;
+            use async_graphql::types::connection::CursorType;
 
             println!("filters: {:?}", filters);
 
@@ -92,194 +70,196 @@ pub fn basic_query(name: &Ident, path: &TokenStream) -> TokenStream {
 
             let stmt = #path::order_by(stmt, order_by);
 
+            fn get_result(
+                data: Vec<#path::Model>,
+                has_previous_page: bool,
+                has_next_page: bool,
+                pages: Option<usize>,
+                current: Option<usize>
+            ) -> async_graphql::types::connection::Connection<
+                String,
+                #path::Model,
+                ExtraPaginationFields,
+                async_graphql::types::connection::EmptyFields
+            > {
+                let edges: Vec<async_graphql::types::connection::Edge<String, #path::Model, async_graphql::types::connection::EmptyFields>> = data
+                    .into_iter()
+                    .map(|node| {
+                        let values: Vec<sea_orm::Value> = #path::PrimaryKey::iter()
+                            .map(|variant| {
+                                node.get(variant.into_column())
+                            })
+                            .collect();
+
+                        let cursor_string = CursorValues(values).encode_cursor();
+
+                        async_graphql::types::connection::Edge::new(cursor_string, node)
+                    })
+                    .collect();
+
+                let mut result = async_graphql::types::connection::Connection::<
+                    String,
+                    #path::Model,
+                    ExtraPaginationFields,
+                    async_graphql::types::connection::EmptyFields
+                >::with_additional_fields(
+                    has_previous_page,
+                    has_next_page,
+                    ExtraPaginationFields {
+                        pages,
+                        current
+                    }
+                );
+
+                result.edges.extend(edges);
+
+                result
+            }
+
             if let Some(pagination) = pagination {
-                let paginator = stmt.paginate(db, pagination.limit);
-                let data: Vec<#path::Model> =
-                    paginator.fetch_page(pagination.page).await.unwrap();
-                let pages = paginator.num_pages().await.unwrap();
-                PaginatedResult {
-                    data,
-                    pages,
-                    current: pagination.page,
+
+                match pagination {
+                    Pagination::Pages(pagination) => {
+                        let paginator = stmt.paginate(db, pagination.limit);
+
+                        let data: Vec<#path::Model> = paginator
+                            .fetch_page(pagination.page)
+                            .await
+                            .unwrap();
+
+                        let pages = paginator
+                            .num_pages()
+                            .await
+                            .unwrap();
+
+                        get_result(data, pagination.page != 1, pagination.page < pages, Some(pages), Some(pagination.page))
+                    },
+                    Pagination::Cursor(cursor) => {
+                        let next_stmt = stmt.clone();
+                        let previous_stmt = stmt.clone();
+
+                        fn apply_stmt_cursor_by(stmt: sea_orm::entity::prelude::Select<#path::Entity>) -> sea_orm::Cursor<sea_orm::SelectModel<#path::Model>> {
+                            if #path::PrimaryKey::iter().len() == 1 {
+                                let column = #path::PrimaryKey::iter().map(|variant| variant.into_column()).collect::<Vec<#path::Column>>()[0];
+                                stmt.cursor_by(column)
+                            } else if #path::PrimaryKey::iter().len() == 2 {
+                                let columns = #path::PrimaryKey::iter().map(|variant| variant.into_column()).collect_tuple::<(#path::Column, #path::Column)>().unwrap();
+                                stmt.cursor_by(columns)
+                            } else if #path::PrimaryKey::iter().len() == 3 {
+                                let columns = #path::PrimaryKey::iter().map(|variant| variant.into_column()).collect_tuple::<(#path::Column, #path::Column, #path::Column)>().unwrap();
+                                stmt.cursor_by(columns)
+                            } else {
+                                panic!("seaography does not support cursors with size greater than 3")
+                            }
+                        }
+
+                        let mut stmt = apply_stmt_cursor_by(stmt);
+
+                        if let Some(cursor_string) = cursor.cursor {
+                            let values = CursorValues::decode_cursor(cursor_string.as_str()).unwrap();
+
+                            let cursor_values: sea_orm::sea_query::value::ValueTuple = map_cursor_values(values.0);
+
+                            stmt.after(cursor_values);
+                        }
+
+                        let data = stmt
+                            .first(cursor.limit)
+                            .all(db)
+                            .await
+                            .unwrap();
+
+                        let has_next_page: bool = {
+                            let mut next_stmt = apply_stmt_cursor_by(next_stmt);
+
+                            let last_node = data.last();
+
+                            if let Some(node) = last_node {
+                                let values: Vec<sea_orm::Value> = #path::PrimaryKey::iter()
+                                    .map(|variant| {
+                                        node.get(variant.into_column())
+                                    })
+                                    .collect();
+
+                                let values = map_cursor_values(values);
+
+                                let next_data = next_stmt
+                                    .first(cursor.limit)
+                                    .after(values)
+                                    .all(db)
+                                    .await
+                                    .unwrap();
+
+                                next_data.len() != 0
+                            } else {
+                                false
+                            }
+                        };
+
+                        let has_previous_page: bool = {
+                            let mut previous_stmt = apply_stmt_cursor_by(previous_stmt);
+
+                            let first_node = data.first();
+
+                            if let Some(node) = first_node {
+                                let values: Vec<sea_orm::Value> = #path::PrimaryKey::iter()
+                                    .map(|variant| {
+                                        node.get(variant.into_column())
+                                    })
+                                    .collect();
+
+                                let values = map_cursor_values(values);
+
+                                let previous_data = previous_stmt
+                                    .first(cursor.limit)
+                                    .before(values)
+                                    .all(db)
+                                    .await
+                                    .unwrap();
+
+                                previous_data.len() != 0
+                            } else {
+                                false
+                            }
+                        };
+
+                        get_result(data, has_previous_page, has_next_page, None, None)
+                    }
                 }
             } else {
                 let data: Vec<#path::Model> = stmt.all(db).await.unwrap();
-                PaginatedResult {
-                    data,
-                    pages: 1,
-                    current: 1,
-                }
+
+                get_result(data, false, false, Some(1), Some(1))
             }
         }
     }
 }
 
-pub fn basic_dependencies(names: Vec<TokenStream>) -> TokenStream {
+pub fn basic_dependencies() -> TokenStream {
     quote! {
         #[derive(Debug, async_graphql::InputObject)]
-        pub struct PaginationInput {
+        pub struct PageInput {
             pub limit: usize,
             pub page: usize,
         }
 
-        #[derive(Debug, async_graphql::SimpleObject)]
-        #(#names)*
-        pub struct PaginatedResult<T: async_graphql::ObjectType> {
-            pub data: Vec<T>,
-            pub pages: usize,
-            pub current: usize,
-        }
-    }
-}
-
-pub fn cursor_query(name: &Ident, path: &TokenStream) -> TokenStream {
-    quote! {
-        pub async fn #name<'a>(
-            &self,
-            ctx: &async_graphql::Context<'a>,
-            filters: Option<#path::Filter>,
-            cursor: CursorPaginationInput,
-            order_by: Option<#path::OrderBy>,
-        ) -> async_graphql::types::connection::Connection<String, #path::Model, async_graphql::types::connection::EmptyFields, async_graphql::types::connection::EmptyFields> {
-            use sea_orm::prelude::*;
-            use sea_orm::Iterable;
-            use seaography::itertools::Itertools;
-            use async_graphql::types::connection::CursorType;
-
-            println!("cursor_filters: {:?}", filters);
-
-            let db: &crate::DatabaseConnection = ctx.data::<crate::DatabaseConnection>().unwrap();
-            let stmt = #path::Entity::find()
-                .filter(#path::filter_recursive(filters));
-
-            let stmt = #path::order_by(stmt, order_by);
-
-            let next_stmt = stmt.clone();
-            let previous_stmt = stmt.clone();
-
-            fn apply_stmt_cursor_by(stmt: sea_orm::entity::prelude::Select<#path::Entity>) -> sea_orm::Cursor<sea_orm::SelectModel<#path::Model>> {
-                if #path::PrimaryKey::iter().len() == 1 {
-                    let column = #path::PrimaryKey::iter().map(|variant| variant.into_column()).collect::<Vec<#path::Column>>()[0];
-                    stmt.cursor_by(column)
-                } else if #path::PrimaryKey::iter().len() == 2 {
-                    let columns = #path::PrimaryKey::iter().map(|variant| variant.into_column()).collect_tuple::<(#path::Column, #path::Column)>().unwrap();
-                    stmt.cursor_by(columns)
-                } else if #path::PrimaryKey::iter().len() == 3 {
-                    let columns = #path::PrimaryKey::iter().map(|variant| variant.into_column()).collect_tuple::<(#path::Column, #path::Column, #path::Column)>().unwrap();
-                    stmt.cursor_by(columns)
-                } else {
-                    panic!("seaography does not support cursors with size greater than 3")
-                }
-            }
-
-            let mut stmt = apply_stmt_cursor_by(stmt);
-
-            if let Some(cursor_string) = cursor.cursor {
-                let values = CursorValues::decode_cursor(cursor_string.as_str()).unwrap();
-
-                let cursor_values: sea_orm::sea_query::value::ValueTuple = map_cursor_values(values.0);
-
-                stmt.after(cursor_values);
-            }
-
-            let data = stmt
-                .first(cursor.limit)
-                .all(db)
-                .await
-                .unwrap();
-
-            let has_next_page: bool = {
-                let mut next_stmt = apply_stmt_cursor_by(next_stmt);
-
-                let last_node = data.last();
-
-                if let Some(node) = last_node {
-                    let values: Vec<sea_orm::Value> = #path::PrimaryKey::iter()
-                        .map(|variant| {
-                            node.get(variant.into_column())
-                        })
-                        .collect();
-
-                    let values = map_cursor_values(values);
-
-                    let next_data = next_stmt
-                        .first(cursor.limit)
-                        .after(values)
-                        .all(db)
-                        .await
-                        .unwrap();
-
-                    next_data.len() != 0
-                } else {
-                    false
-                }
-            };
-
-            let has_previous_page: bool = {
-                let mut previous_stmt = apply_stmt_cursor_by(previous_stmt);
-
-                let first_node = data.first();
-
-                if let Some(node) = first_node {
-                    let values: Vec<sea_orm::Value> = #path::PrimaryKey::iter()
-                        .map(|variant| {
-                            node.get(variant.into_column())
-                        })
-                        .collect();
-
-                    let values = map_cursor_values(values);
-
-                    let previous_data = previous_stmt
-                        .first(cursor.limit)
-                        .before(values)
-                        .all(db)
-                        .await
-                        .unwrap();
-
-                    previous_data.len() != 0
-                } else {
-                    false
-                }
-            };
-
-            let edges: Vec<async_graphql::types::connection::Edge<String, #path::Model, async_graphql::connection::EmptyFields>> = data
-                .into_iter()
-                .map(|node| {
-                    let values: Vec<sea_orm::Value> = #path::PrimaryKey::iter()
-                        .map(|variant| {
-                            node.get(variant.into_column())
-                        })
-                        .collect();
-
-                    let cursor_string = CursorValues(values).encode_cursor();
-
-                    async_graphql::types::connection::Edge::new(cursor_string, node)
-                })
-                .collect();
-
-            let mut result = async_graphql::types::connection::Connection::<
-                String,
-                #path::Model,
-                async_graphql::types::connection::EmptyFields,
-                async_graphql::types::connection::EmptyFields
-            >::new(
-                has_previous_page,
-                has_next_page
-            );
-
-            result.edges.extend(edges);
-
-            result
-        }
-    }
-}
-
-pub fn cursor_dependencies() -> TokenStream {
-    quote! {
         #[derive(Debug, async_graphql::InputObject)]
-        pub struct CursorPaginationInput {
+        pub struct CursorInput {
             pub cursor: Option<String>,
             pub limit: u64,
+        }
+
+        #[derive(async_graphql::OneofObject)]
+        pub enum Pagination {
+            Pages(PageInput),
+            Cursor(CursorInput),
+        }
+
+
+        #[derive(async_graphql::SimpleObject)]
+        pub struct ExtraPaginationFields {
+            pub pages: Option<usize>,
+            pub current: Option<usize>,
         }
 
         #[derive(Debug)]
