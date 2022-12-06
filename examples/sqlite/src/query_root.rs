357e925eaf0803e5e4d565693da62c6d8b2a6b07
Panagiotis Karatakis <karatakis@csd.auth.gr>
2022-12-06 21:50:01 +0200

    Add filter and orderBy object generation

--- examples/sqlite/src/query_root.rs
+++ examples/sqlite/src/query_root.rs
@@ -13,13 +13,15 @@ pub fn schema(
     let actor = entity_to_dynamic_graphql::<crate::entities::actor::Entity>();
     let address = entity_to_dynamic_graphql::<crate::entities::address::Entity>();
 
-    let query = Object::new("Query")
-        .field(actor.query)
-        .field(address.query);
+    let query = Object::new("Query").field(actor.query).field(address.query);
 
     let schema = Schema::build(query.type_name(), None, None)
         .register(actor.object)
+        .register(actor.filter)
+        .register(actor.order)
         .register(address.object)
+        .register(address.filter)
+        .register(address.order)
         .register(query);
 
     let schema = if let Some(depth) = depth {
@@ -27,6 +29,10 @@ pub fn schema(
     } else {
         schema
     };
+
+    let schema = get_filter_types()
+        .into_iter()
+        .fold(schema, |schema, object| schema.register(object));
 
     let schema = if let Some(complexity) = complexity {
         schema.limit_complexity(complexity)
@@ -40,6 +46,8 @@ pub fn schema(
 pub struct DynamicGraphqlEntity {
     pub object: Object,
     pub query: Field,
+    pub filter: InputObject,
+    pub order: InputObject,
 }
 
 pub fn entity_to_dynamic_graphql<T>() -> DynamicGraphqlEntity
@@ -49,9 +57,18 @@ where
 {
     let object = entity_to_object::<T>();
 
-    let query = entity_to_query::<T>(&object);
+    let filter = entity_to_filter::<T>();
 
-    DynamicGraphqlEntity { object, query }
+    let order = entity_to_order::<T>();
+
+    let query = entity_to_query::<T>(&object, &filter, &order);
+
+    DynamicGraphqlEntity {
+        object,
+        query,
+        filter,
+        order,
+    }
 }
 
 pub fn entity_to_object<T>() -> Object
@@ -286,7 +303,171 @@ where
     )
 }
 
-pub fn entity_to_query<T>(object: &Object) -> Field
+#[macro_export]
+macro_rules! basic_filter_input_type {
+    ( $name:literal, $type:expr ) => {
+        InputObject::new($name)
+            .field(InputValue::new("eq", TypeRef::named($type)))
+            .field(InputValue::new("ne", TypeRef::named($type)))
+            .field(InputValue::new("gt", TypeRef::named($type)))
+            .field(InputValue::new("gte", TypeRef::named($type)))
+            .field(InputValue::new("lt", TypeRef::named($type)))
+            .field(InputValue::new("lte", TypeRef::named($type)))
+            .field(InputValue::new("is_in", TypeRef::named_nn_list($type)))
+            .field(InputValue::new("is_not_in", TypeRef::named_nn_list($type)))
+            .field(InputValue::new("is_null", TypeRef::named(TypeRef::BOOLEAN)))
+    };
+}
+
+pub fn get_filter_types() -> Vec<InputObject> {
+    vec![
+        basic_filter_input_type!("TextFilterInput", TypeRef::STRING),
+        InputObject::new("StringFilterInput")
+            .field(InputValue::new("eq", TypeRef::named(TypeRef::STRING)))
+            .field(InputValue::new("ne", TypeRef::named(TypeRef::STRING)))
+            .field(InputValue::new("gt", TypeRef::named(TypeRef::STRING)))
+            .field(InputValue::new("gte", TypeRef::named(TypeRef::STRING)))
+            .field(InputValue::new("lt", TypeRef::named(TypeRef::STRING)))
+            .field(InputValue::new("lte", TypeRef::named(TypeRef::STRING)))
+            .field(InputValue::new(
+                "is_in",
+                TypeRef::named_nn_list(TypeRef::STRING),
+            ))
+            .field(InputValue::new(
+                "is_not_in",
+                TypeRef::named_nn_list(TypeRef::STRING),
+            ))
+            .field(InputValue::new("is_null", TypeRef::named(TypeRef::BOOLEAN)))
+            .field(InputValue::new("contains", TypeRef::named(TypeRef::STRING)))
+            .field(InputValue::new(
+                "starts_with",
+                TypeRef::named(TypeRef::STRING),
+            ))
+            .field(InputValue::new(
+                "ends_with",
+                TypeRef::named(TypeRef::STRING),
+            ))
+            .field(InputValue::new("like", TypeRef::named(TypeRef::STRING)))
+            .field(InputValue::new("not_like", TypeRef::named(TypeRef::STRING))),
+        basic_filter_input_type!("IntegerFilterInput", TypeRef::INT),
+        basic_filter_input_type!("FloatFilterInput", TypeRef::FLOAT),
+        basic_filter_input_type!("BooleanFilterInput", TypeRef::BOOLEAN),
+        basic_filter_input_type!("IdFilterInput", TypeRef::ID),
+    ]
+}
+
+pub fn entity_to_filter<T>() -> InputObject
+where
+    T: sea_orm::EntityTrait,
+    <T as sea_orm::EntityTrait>::Model: Sync,
+{
+    let name = format!(
+        "{}FilterInput",
+        <T as sea_orm::EntityName>::table_name(&T::default())
+    )
+    .to_upper_camel_case();
+
+    let object = T::Column::iter().fold(InputObject::new(&name), |object, column| {
+        let field = match column.def().get_column_type() {
+            ColumnType::Char(_) | ColumnType::String(_) | ColumnType::Text => {
+                InputValue::new(column.as_str(), TypeRef::named("StringFilterInput"))
+            }
+            ColumnType::TinyInteger
+            | ColumnType::SmallInteger
+            | ColumnType::Integer
+            | ColumnType::BigInteger
+            | ColumnType::TinyUnsigned
+            | ColumnType::SmallUnsigned
+            | ColumnType::Unsigned
+            | ColumnType::BigUnsigned => {
+                InputValue::new(column.as_str(), TypeRef::named("IntegerFilterInput"))
+            }
+            ColumnType::Float | ColumnType::Double => {
+                InputValue::new(column.as_str(), TypeRef::named("FloatFilterInput"))
+            }
+            ColumnType::Decimal(_) => {
+                InputValue::new(column.as_str(), TypeRef::named("TextFilterInput"))
+            }
+            // ColumnType::DateTime => {
+
+            // },
+            // ColumnType::Timestamp => {
+
+            // },
+            // ColumnType::TimestampWithTimeZone => {
+
+            // },
+            // ColumnType::Time => {
+
+            // },
+            // ColumnType::Date => {
+
+            // },
+            // ColumnType::Binary => {
+
+            // },
+            // ColumnType::TinyBinary => {
+
+            // },
+            // ColumnType::MediumBinary => {
+
+            // },
+            // ColumnType::LongBinary => {
+
+            // },
+            // ColumnType::Boolean => {
+
+            // },
+            // ColumnType::Money(_) => {
+
+            // },
+            // ColumnType::Json => {
+
+            // },
+            // ColumnType::JsonBinary => {
+
+            // },
+            // ColumnType::Custom(_) => {
+
+            // },
+            // ColumnType::Uuid => {
+
+            // },
+            // ColumnType::Enum { name, variants } => {
+
+            // },
+            // ColumnType::Array(_) => {
+
+            // },
+            _ => InputValue::new(column.as_str(), TypeRef::named("TextFilterInput")),
+        };
+
+        object.field(field)
+    });
+
+    object
+        .field(InputValue::new("and", TypeRef::named_nn_list(&name)))
+        .field(InputValue::new("or", TypeRef::named_nn_list(&name)))
+}
+
+pub fn entity_to_order<T>() -> InputObject
+where
+    T: sea_orm::EntityTrait,
+    <T as sea_orm::EntityTrait>::Model: Sync,
+{
+    let name = format!(
+        "{}OrderInput",
+        <T as sea_orm::EntityName>::table_name(&T::default())
+    )
+    .to_upper_camel_case();
+
+    T::Column::iter().fold(InputObject::new(&name), |object, column| {
+
+        object.field(InputValue::new(column.as_str(),TypeRef::named(TypeRef::BOOLEAN)))
+    })
+}
+
+pub fn entity_to_query<T>(object: &Object, filter: &InputObject, order: &InputObject) -> Field
 where
     T: sea_orm::EntityTrait,
     <T as sea_orm::EntityTrait>::Model: Sync,
@@ -295,7 +476,22 @@ where
         format!("{}s", <T as sea_orm::EntityName>::table_name(&T::default())),
         TypeRef::named_list(object.type_name()),
         move |ctx| {
+
             FieldFuture::new(async move {
+                let filters = ctx.args.try_get("filters");
+
+                if let Ok(v) = filters {
+                    v.object()?
+                        .iter()
+                        .for_each(|(name, val)| {
+                            println!("{:?}", name);
+                            println!("{:?}", val.object().unwrap().try_get("eq").unwrap().i64());
+                        });
+                } else {
+                    println!("KEY filters is MISSING!")
+                };
+
+
                 let database = ctx.data::<DatabaseConnection>()?;
                 let data = T::find().all(database).await?;
                 Ok(Some(FieldValue::list(
@@ -304,4 +500,12 @@ where
             })
         },
     )
+    .argument(InputValue::new(
+        "filters",
+        TypeRef::named(filter.type_name()),
+    ))
+    .argument(InputValue::new(
+        "orderBy",
+        TypeRef::named(order.type_name()),
+    ))
 }
