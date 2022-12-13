1dbf8ac3b6871db66054fdc015fe6920600d4525
Panagiotis Karatakis <karatakis@csd.auth.gr>
2022-12-14 01:59:21 +0200

    WIP filtering

--- examples/sqlite/src/query_root.rs
+++ examples/sqlite/src/query_root.rs
@@ -1,5 +1,5 @@
 use async_graphql::{dataloader::DataLoader, dynamic::*, Value};
-use sea_orm::{prelude::*, Iterable};
+use sea_orm::{prelude::*, Condition, Iterable};
 use seaography::heck::ToUpperCamelCase;
 
 use crate::OrmDataloader;
@@ -17,11 +17,11 @@ pub fn schema(
 
     let schema = Schema::build(query.type_name(), None, None)
         .register(actor.object)
-        .register(actor.filter)
-        .register(actor.order)
+        .register(actor.filter_input)
+        .register(actor.order_input)
         .register(address.object)
-        .register(address.filter)
-        .register(address.order)
+        .register(address.filter_input)
+        .register(address.order_input)
         .register(query);
 
     let schema = if let Some(depth) = depth {
@@ -46,38 +46,38 @@ pub fn schema(
 pub struct DynamicGraphqlEntity {
     pub object: Object,
     pub query: Field,
-    pub filter: InputObject,
-    pub order: InputObject,
+    pub filter_input: InputObject,
+    pub order_input: InputObject,
 }
 
 pub fn entity_to_dynamic_graphql<T>() -> DynamicGraphqlEntity
 where
-    T: sea_orm::EntityTrait,
-    <T as sea_orm::EntityTrait>::Model: Sync,
+    T: EntityTrait,
+    <T as EntityTrait>::Model: Sync,
 {
     let object = entity_to_object::<T>();
 
-    let filter = entity_to_filter::<T>();
+    let filter_input = entity_to_filter::<T>();
 
-    let order = entity_to_order::<T>();
+    let order_input = entity_to_order::<T>();
 
-    let query = entity_to_query::<T>(&object, &filter, &order);
+    let query = entity_to_query::<T>(&object, &filter_input, &order_input);
 
     DynamicGraphqlEntity {
         object,
         query,
-        filter,
-        order,
+        filter_input,
+        order_input,
     }
 }
 
 pub fn entity_to_object<T>() -> Object
 where
-    T: sea_orm::EntityTrait,
-    <T as sea_orm::EntityTrait>::Model: Sync,
+    T: EntityTrait,
+    <T as EntityTrait>::Model: Sync,
 {
     T::Column::iter().fold(
-        Object::new(<T as sea_orm::EntityName>::table_name(&T::default()).to_upper_camel_case()),
+        Object::new(<T as EntityName>::table_name(&T::default()).to_upper_camel_case()),
         |object, column| {
             let field = match column.def().get_column_type() {
                 ColumnType::Char(_) => Field::new(
@@ -358,12 +358,12 @@ pub fn get_filter_types() -> Vec<InputObject> {
 
 pub fn entity_to_filter<T>() -> InputObject
 where
-    T: sea_orm::EntityTrait,
-    <T as sea_orm::EntityTrait>::Model: Sync,
+    T: EntityTrait,
+    <T as EntityTrait>::Model: Sync,
 {
     let name = format!(
         "{}FilterInput",
-        <T as sea_orm::EntityName>::table_name(&T::default())
+        <T as EntityName>::table_name(&T::default())
     )
     .to_upper_camel_case();
 
@@ -452,45 +452,49 @@ where
 
 pub fn entity_to_order<T>() -> InputObject
 where
-    T: sea_orm::EntityTrait,
-    <T as sea_orm::EntityTrait>::Model: Sync,
+    T: EntityTrait,
+    <T as EntityTrait>::Model: Sync,
 {
-    let name = format!(
-        "{}OrderInput",
-        <T as sea_orm::EntityName>::table_name(&T::default())
-    )
-    .to_upper_camel_case();
+    let name =
+        format!("{}OrderInput", <T as EntityName>::table_name(&T::default())).to_upper_camel_case();
 
     T::Column::iter().fold(InputObject::new(&name), |object, column| {
-
-        object.field(InputValue::new(column.as_str(),TypeRef::named(TypeRef::BOOLEAN)))
+        object.field(InputValue::new(
+            column.as_str(),
+            TypeRef::named(TypeRef::BOOLEAN),
+        ))
     })
 }
 
-pub fn entity_to_query<T>(object: &Object, filter: &InputObject, order: &InputObject) -> Field
+pub fn entity_to_query<T>(
+    object: &Object,
+    filter_input: &InputObject,
+    order_input: &InputObject,
+) -> Field
 where
-    T: sea_orm::EntityTrait,
-    <T as sea_orm::EntityTrait>::Model: Sync,
+    T: EntityTrait,
+    <T as EntityTrait>::Model: Sync,
 {
     Field::new(
-        format!("{}s", <T as sea_orm::EntityName>::table_name(&T::default())),
+        format!("{}s", <T as EntityName>::table_name(&T::default())),
         TypeRef::named_list(object.type_name()),
         move |ctx| {
-
             FieldFuture::new(async move {
-                let filters = ctx.args.try_get("filters");
+                let filters = ctx.args.get("filters");
 
-                if let Ok(v) = filters {
-                    v.object()?
-                        .iter()
-                        .for_each(|(name, val)| {
-                            println!("{:?}", name);
-                            println!("{:?}", val.object().unwrap().try_get("eq").unwrap().i64());
-                        });
-                } else {
-                    println!("KEY filters is MISSING!")
-                };
+                // if let Ok(v) = filters {
+                //     v.object()?
+                //         .iter()
+                //         .for_each(|(name, val)| {
+                //             println!("{:?}", name);
+                //             println!("{:?}", val.object().unwrap().try_get("eq").unwrap().i64());
+                //         });
+                // } else {
+                //     println!("KEY filters is MISSING!")
+                // };
 
+                let stmt = T::find();
+                let stmt = apply_filters(stmt, filters);
 
                 let database = ctx.data::<DatabaseConnection>()?;
                 let data = T::find().all(database).await?;
@@ -502,10 +506,205 @@ where
     )
     .argument(InputValue::new(
         "filters",
-        TypeRef::named(filter.type_name()),
+        TypeRef::named(filter_input.type_name()),
     ))
     .argument(InputValue::new(
         "orderBy",
-        TypeRef::named(order.type_name()),
+        TypeRef::named(order_input.type_name()),
     ))
+}
+
+#[macro_export]
+macro_rules! basic_filtering_operation {
+    ( $condition:expr, $column:expr, $filter:expr, $operator:ident, $type:ident ) => {
+        if let Some(data) = $filter.get(stringify!($operator)) {
+            let data = data.$type().expect(format!(
+                "We expect the {} to be of type {}",
+                stringify!($operator),
+                stringify!($type)
+            ).as_str());
+
+            $condition.add($column.$operator(data))
+        } else {
+            $condition
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! basic_filtering_type {
+    ( $condition:expr, $column:expr, $filter:expr, $type:ident ) => {
+        {
+            let condition = basic_filtering_operation!($condition, $column, $filter, eq, $type);
+            let condition = basic_filtering_operation!(condition, $column, $filter, ne, $type);
+            let condition = basic_filtering_operation!(condition, $column, $filter, gt, $type);
+            let condition = basic_filtering_operation!(condition, $column, $filter, gte, $type);
+            let condition = basic_filtering_operation!(condition, $column, $filter, lt, $type);
+            let condition = basic_filtering_operation!(condition, $column, $filter, lte, $type);
+            // let condition = basic_filtering_operation!(condition, $column, $filter, is_in, $type);
+            // let condition = basic_filtering_operation!(condition, $column, $filter, is_not_in, $type);
+            // let condition = basic_filtering_operation!(condition, $column, $filter, is_null, $type);
+
+            condition
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! string_filtering_type {
+    ( $condition:expr, $column:expr, $filter:expr, $type:ident ) => {
+        {
+            let condition = basic_filtering_operation!($condition, $column, $filter, eq, $type);
+            let condition = basic_filtering_operation!(condition, $column, $filter, ne, $type);
+            let condition = basic_filtering_operation!(condition, $column, $filter, gt, $type);
+            let condition = basic_filtering_operation!(condition, $column, $filter, gte, $type);
+            let condition = basic_filtering_operation!(condition, $column, $filter, lt, $type);
+            let condition = basic_filtering_operation!(condition, $column, $filter, lte, $type);
+            // let condition = basic_filtering_operation!(condition, $column, $filter, is_in, $type);
+            // let condition = basic_filtering_operation!(condition, $column, $filter, is_not_in, $type);
+            // let condition = basic_filtering_operation!(condition, $column, $filter, is_null, $type);
+            let condition = basic_filtering_operation!(condition, $column, $filter, contains, $type);
+            let condition = basic_filtering_operation!(condition, $column, $filter, starts_with, $type);
+            let condition = basic_filtering_operation!(condition, $column, $filter, ends_with, $type);
+            let condition = basic_filtering_operation!(condition, $column, $filter, like, $type);
+            let condition = basic_filtering_operation!(condition, $column, $filter, not_like, $type);
+
+            condition
+        }
+    };
+}
+
+pub fn apply_filters<T>(
+    stmt: Select<T>,
+    filters: Option<ValueAccessor>,
+) -> Result<Select<T>, async_graphql::Error>
+where
+    T: EntityTrait,
+    <T as EntityTrait>::Model: Sync,
+{
+    if let Some(filters) = filters {
+        let filters = filters
+            .object()
+            .expect("We expect the entity filters to be object type");
+
+        let condition = recursive_prepare_condition::<T>(filters);
+
+        println!("Condition: {:?}", condition);
+
+        Ok(stmt.filter(condition))
+    } else {
+        Ok(stmt)
+    }
+}
+
+pub fn recursive_prepare_condition<T>(filters: ObjectAccessor) -> Condition
+where
+    T: EntityTrait,
+    <T as EntityTrait>::Model: Sync,
+{
+    let condition = T::Column::iter().fold(Condition::all(), |condition, column: T::Column| {
+        let filter = filters.get(column.as_str());
+
+        if let Some(filter) = filter {
+            let filter = filter
+                .object()
+                .expect("We expect the column filter to be object type");
+
+            match column.def().get_column_type() {
+                ColumnType::Char(_) | ColumnType::String(_) | ColumnType::Text => string_filtering_type!(condition, column, filter, string),
+                ColumnType::TinyInteger
+                | ColumnType::SmallInteger
+                | ColumnType::Integer
+                | ColumnType::BigInteger => basic_filtering_type!(condition, column, filter, i64),
+                ColumnType::TinyUnsigned
+                | ColumnType::SmallUnsigned
+                | ColumnType::Unsigned
+                | ColumnType::BigUnsigned => basic_filtering_type!(condition, column, filter, u64),
+                ColumnType::Float | ColumnType::Double => basic_filtering_type!(condition, column, filter, f64),
+                ColumnType::Decimal(_) => basic_filtering_type!(condition, column, filter, string),
+                // ColumnType::DateTime => {
+
+                // },
+                // ColumnType::Timestamp => {
+
+                // },
+                // ColumnType::TimestampWithTimeZone => {
+
+                // },
+                // ColumnType::Time => {
+
+                // },
+                // ColumnType::Date => {
+
+                // },
+                // ColumnType::Binary => {
+
+                // },
+                // ColumnType::TinyBinary => {
+
+                // },
+                // ColumnType::MediumBinary => {
+
+                // },
+                // ColumnType::LongBinary => {
+
+                // },
+                // ColumnType::Boolean => {
+
+                // },
+                // ColumnType::Money(_) => {
+
+                // },
+                // ColumnType::Json => {
+
+                // },
+                // ColumnType::JsonBinary => {
+
+                // },
+                // ColumnType::Custom(_) => {
+
+                // },
+                // ColumnType::Uuid => {
+
+                // },
+                // ColumnType::Enum { name, variants } => {
+
+                // },
+                // ColumnType::Array(_) => {
+
+                // },
+                _ => panic!("Type is not supported"),
+            }
+        } else {
+            condition
+        }
+    });
+
+    let condition = if let Some(and) = filters.get("and") {
+        let filters = and.list().expect("We expect to find a list of FiltersInput");
+
+        condition.add(
+            filters.iter().fold(Condition::all(), |condition, filters: ValueAccessor| {
+                let filters = filters.object().expect("We expect an FiltersInput object");
+                condition.add(recursive_prepare_condition::<T>(filters))
+            })
+        )
+    } else {
+        condition
+    };
+
+    let condition = if let Some(or) = filters.get("or") {
+        let filters = or.list().expect("We expect to find a list of FiltersInput");
+
+        condition.add(
+            filters.iter().fold(Condition::any(), |condition, filters: ValueAccessor| {
+                let filters = filters.object().expect("We expect an FiltersInput object");
+                condition.add(recursive_prepare_condition::<T>(filters))
+            })
+        )
+    } else {
+        condition
+    };
+
+    condition
 }
