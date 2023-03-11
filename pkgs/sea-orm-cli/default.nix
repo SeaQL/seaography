4123dcd088b5591a2c6be925422f75d964697861
campbellcole <10430178+campbellcole@users.noreply.github.com>
2023-03-11 02:06:50 -0800

    fix incorrect package metadata

--- pkgs/sea-orm-cli/default.nix
+++ pkgs/sea-orm-cli/default.nix
@@ -14,8 +14,8 @@ rustPlatform.buildRustPackage rec {
     cargoSha256 = "sha256-qJ+G5+KyoNsl3VKsHDdFD6l1MagqHd7N7TahK7B5ky8=";
 
     meta = with lib; {
-        description = "Rapidly scaffold out a new tauri app project.";
-        homepage = "https://github.com/tauri-apps/create-tauri-app";
+        description = "Command line utility for SeaORM";
+        homepage = "https://github.com/SeaQL/sea-orm/tree/master/sea-orm-cli";
         license = with licenses; [ mit asl20 ];
     };
 }
