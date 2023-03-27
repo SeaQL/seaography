6499d5228e09f15b869d38b63bd902fabb722464
Billy Chan <ccw.billy.123@gmail.com>
2023-03-27 22:57:59 +0800

    add tests

--- examples/mysql/src/query_root.rs
+++ examples/mysql/src/query_root.rs
@@ -14,5 +14,35 @@
 #[seaography(entity = "crate::entities::payment")]
 #[seaography(entity = "crate::entities::rental")]
 #[seaography(entity = "crate::entities::staff")]
-#[seaography(entity = "crate::entities::store")]
+#[seaography(
+    entity = "crate::entities::store",
+    object_config = "guard = \"RoleGuard::new(Role::Admin)\""
+)]
 pub struct QueryRoot;
+
+#[derive(Eq, PartialEq, Copy, Clone)]
+enum Role {
+    Admin,
+    Guest,
+}
+
+struct RoleGuard {
+    role: Role,
+}
+
+impl RoleGuard {
+    fn new(role: Role) -> Self {
+        Self { role }
+    }
+}
+
+#[async_trait::async_trait]
+impl async_graphql::Guard for RoleGuard {
+    async fn check(&self, ctx: &async_graphql::Context<'_>) -> async_graphql::Result<()> {
+        if ctx.data_opt::<Role>() == Some(&self.role) {
+            Ok(())
+        } else {
+            Err("Forbidden".into())
+        }
+    }
+}
