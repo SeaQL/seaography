3f4c71af9c17cbecae793b6beb35dbcee6860f71
Billy Chan <ccw.billy.123@gmail.com>
2024-05-03 18:42:57 +0800

    Axum

--- generator/src/templates/axum.rs
+++ generator/src/templates/axum.rs
@@ -10,21 +10,18 @@ pub fn generate_main(crate_name: &str) -> TokenStream {
     let crate_name_token: TokenStream = crate_name.replace('-', "_").parse().unwrap();
 
     quote! {
-        use dotenv::dotenv;
-        use lazy_static::lazy_static;
-        use sea_orm::Database;
-        use async_graphql::{
-            http::{playground_source, GraphQLPlaygroundConfig},
-            dynamic::*,
-        };
+        use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
         use async_graphql_axum::GraphQL;
         use axum::{
             response::{self, IntoResponse},
             routing::get,
             Router,
         };
-        use tokio::net::TcpListener;
+        use dotenv::dotenv;
+        use lazy_static::lazy_static;
+        use sea_orm::Database;
         use std::env;
+        use tokio::net::TcpListener;
 
         lazy_static! {
             static ref URL: String = env::var("URL").unwrap_or("localhost:8000".into());
@@ -41,7 +38,7 @@ pub fn generate_main(crate_name: &str) -> TokenStream {
         }
 
         async fn graphiql() -> impl IntoResponse {
-            response::Html(playground_source(GraphQLPlaygroundConfig::new("http://localhost:8000")))
+            response::Html(playground_source(GraphQLPlaygroundConfig::new(&*ENDPOINT)))
         }
 
         #[tokio::main]
@@ -54,13 +51,9 @@ pub fn generate_main(crate_name: &str) -> TokenStream {
             let database = Database::connect(&*DATABASE_URL)
                 .await
                 .expect("Fail to initialize database connection");
-
             let schema = #crate_name_token::query_root::schema(database, *DEPTH_LIMIT, *COMPLEXITY_LIMIT).unwrap();
-
             let app = Router::new().route("/", get(graphiql).post_service(GraphQL::new(schema)));
-
             println!("Visit GraphQL Playground at http://{}", *URL);
-
             axum::serve(TcpListener::bind(&*URL).await.unwrap(), app)
                 .await
                 .unwrap();
