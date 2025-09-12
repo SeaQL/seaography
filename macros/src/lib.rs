d38589679c2952a06e8c99901cfd55f663c8b4fb
Peter Kelly <peter@pmkelly.net>
2025-09-12 11:49:15 +0700

    Remove old macros

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -3,46 +3,13 @@ extern crate proc_macro;
 
 use darling::FromDeriveInput;
 use proc_macro::TokenStream;
-use syn::{DeriveInput, ItemImpl, parse_macro_input};
+use syn::{DeriveInput, ItemImpl};
 
 mod custom_enum;
 mod custom_fields;
-mod custom_input;
 mod custom_input_type;
-mod custom_operation;
-mod custom_output;
 mod custom_output_type;
 mod util;
-
-#[proc_macro_derive(CustomOperation, attributes(seaography))]
-pub fn custom_operation(input: TokenStream) -> TokenStream {
-    let derive_input: DeriveInput = parse_macro_input!(input);
-
-    match custom_operation::expand(derive_input) {
-        Ok(token_stream) => token_stream.into(),
-        Err(e) => e.to_compile_error().into(),
-    }
-}
-
-#[proc_macro_derive(CustomInput, attributes(seaography))]
-pub fn custom_input(input: TokenStream) -> TokenStream {
-    let derive_input: DeriveInput = parse_macro_input!(input);
-
-    match custom_input::expand(derive_input) {
-        Ok(token_stream) => token_stream.into(),
-        Err(e) => e.to_compile_error().into(),
-    }
-}
-
-#[proc_macro_derive(CustomOutput, attributes(seaography))]
-pub fn custom_output(input: TokenStream) -> TokenStream {
-    let derive_input: DeriveInput = parse_macro_input!(input);
-
-    match custom_output::expand(derive_input) {
-        Ok(token_stream) => token_stream.into(),
-        Err(e) => e.to_compile_error().into(),
-    }
-}
 
 #[derive(Debug, FromDeriveInput)]
 #[darling(attributes(seaography))]
