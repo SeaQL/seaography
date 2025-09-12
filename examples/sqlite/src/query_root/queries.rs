ca1d70b5286b2c4ca923c1a12e33d93f343a0a36
Peter Kelly <peter@pmkelly.net>
2025-09-12 11:43:59 +0700

    Remove commented-out code from custom tests

--- examples/sqlite/src/query_root/queries.rs
+++ examples/sqlite/src/query_root/queries.rs
@@ -3,25 +3,6 @@ use async_graphql::Result as GqlResult;
 use sea_orm::{ColumnTrait, EntityTrait, QueryFilter, QuerySelect};
 use seaography::{apply_pagination, macros::CustomOperation, Connection, PaginationInput};
 
-/*
-
-{
-  customer_of_store2(pagination: { page: { page: 0, limit: 1 } }) {
-    nodes {
-      storeId
-      customerId
-      lastName
-      email
-    }
-    paginationInfo {
-      pages
-      current
-    }
-  }
-}
-
-*/
-
 #[rustfmt::skip]
 #[allow(dead_code)]
 #[derive(CustomOperation)]
@@ -73,34 +54,3 @@ impl Operations {
         })
     }
 }
-
-/*
-fn customer_of_store2() -> Field {
-    use seaography::GqlModelType;
-
-    Field::new(
-        "customer_of_store2",
-        seaography::Connection::<customer::Entity>::gql_type_ref(&CONTEXT),
-        move |ctx| {
-            FieldFuture::new(async move {
-                use sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
-                use seaography::{apply_pagination, PaginationInputBuilder};
-
-                let db = ctx.data::<DatabaseConnection>()?;
-
-                let stmt = customer::Entity::find().filter(customer::Column::StoreId.eq(2));
-                let pagination = ctx.args.get("pagination");
-                let pagination =
-                    PaginationInputBuilder { context: &CONTEXT }.parse_object(pagination);
-                let connection = apply_pagination::<customer::Entity>(db, stmt, pagination).await?;
-
-                Ok(Some(FieldValue::owned_any(connection)))
-            })
-        },
-    )
-    .argument(InputValue::new(
-        "pagination",
-        seaography::PaginationInput::gql_type_ref(&CONTEXT),
-    ))
-}
-*/
